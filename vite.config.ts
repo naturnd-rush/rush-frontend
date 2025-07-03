7bd419e6c5682ed077ea13f8494db16ceaaaab61
Doug Johnson <doug@stormtree.ca>
2025-07-02 20:44:41 -0700

    Add Linaria CSS and Stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import wyw from '@wyw-in-js/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,9 @@ export default defineConfig({
       target: 'react',
       autoCodeSplitting: true,
     }),
-    react()
+    react(),
+    wyw({
+      include: ['**/*.{ts,tsx}'],
+    }),
   ],
 })
