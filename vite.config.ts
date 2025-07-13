a3b462effb9b621b6cb6af6da4333592d417e71b
Doug Johnson <doug@stormtree.ca>
2025-07-12 17:37:56 -0700

    Configure import paths and restrictions

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import wyw from '@wyw-in-js/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,5 +17,6 @@ export default defineConfig({
     wyw({
       include: ['**/*.{ts,tsx}'],
     }),
+    tsconfigPaths(),
   ],
 })
