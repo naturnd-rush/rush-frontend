c3b034ec703344e2a4360b5a759e6898f070bdd2
Doug Johnson <doug@stormtree.ca>
2025-07-22 11:25:26 -0700

    Connect GraphQL and move providers to src/lib

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,5 +1,6 @@
-import TopicCard from '@/features/topic/components/topic-card'
 import { createFileRoute } from '@tanstack/react-router'
+import { useQuery, gql } from '@apollo/client'
+import TopicCard, { type Topic } from '@/features/topic/components/topic-card'
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -12,11 +13,41 @@ const MOCK_TOPIC_DATA = {
   subtitle: 'Personal and planetary health are the same thing. How can I keep my natural life support system connected and protected?',
 }
 
+const GET_TOPICS = gql`
+  query GetQuestions {
+    allQuestions {
+      id
+      title
+    }
+  }
+`
+
+function DisplayTopics() {
+  const { loading, error, data } = useQuery(GET_TOPICS);
+  const topicData: Topic = {
+    id: "",
+    title: "",
+    image: "",
+    subtitle: "",
+  }
+
+  if (loading) { topicData.title = "Loading..." }
+  else if (error) { topicData.title = `Error: ${error.message}`}
+  else {
+    topicData.title = data.allQuestions[0].title
+    topicData.id = data.allQuestions[0].id
+    topicData.image = MOCK_TOPIC_DATA.image
+    topicData.subtitle = MOCK_TOPIC_DATA.subtitle
+  }
+
+  return topicData
+}
+
 function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
-      <TopicCard topic={MOCK_TOPIC_DATA} />
+      <TopicCard topic={DisplayTopics()} />
     </div>
   )
 }
