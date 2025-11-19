859d56dabaf31ab789c547e7c76c6edb271c13e3
Doug Johnson <doug@stormtree.ca>
2025-11-18 22:59:54 -0800

    Portal groups and layers to legend

--- src/app/routes/app/$topicId/route.tsx
+++ src/app/routes/app/$topicId/route.tsx
@@ -1,52 +1,24 @@
 import Spacer from '@/components/spacer'
-import LayerController from '@/features/map/components/layer-controller'
 import Legend from '@/features/map/components/legend'
 import LegendGroup from '@/features/map/components/legend-group'
 import MapControlOverlay, { MapControl } from '@/features/map/components/map-control-overlay'
 import MapView from '@/features/map/components/map-view'
 import { useTopicLayers } from '@/features/map/hooks/use-topic-layers'
 import { byDisplayOrder } from '@/lib/GraphQLProvider'
-import type { OrderedLayerDisplay, OrderedLayerGroup } from '@/types/layers'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
-import { useRef } from 'react'
 
 export const Route = createFileRoute('/app/$topicId')({
   component: RouteComponent,
 })
-
-type LayerWithGroupId = OrderedLayerDisplay & { groupNode: HTMLElement | null }
-
-const layerGroupToLayersAndGroups = (layerGroups: OrderedLayerGroup[]) => {
-  let layers: LayerWithGroupId[] = []
-
-  layerGroups.sort(byDisplayOrder)
-  const layerGroupsForLegend = layerGroups?.map((group) => {
-    const { layers: groupLayers, ...groupDetails } = group
-    const groupNode = useRef(null)
-    const groupElement = <LegendGroup {...groupDetails} ref={groupNode} />
-
-    // flatten layers to external array
-    groupLayers.forEach((layer) => {
-      const layerWithGroupId: LayerWithGroupId = { ...layer, groupNode: groupNode.current }
-      layers.push(layerWithGroupId)
-    })
-    
-    return groupElement
-  })
-
-  layers.sort(byDisplayOrder)
-
-  return { layers, groups: layerGroupsForLegend }
-}
 
 function RouteComponent() {
   const { topicId } = Route.useParams()
   
   // Map Layer API Call
   const [ loading, error, layerGroups ] = useTopicLayers(topicId)
-  const { layers, groups } = layerGroups
-    ? layerGroupToLayersAndGroups(layerGroups)
-    : { layers: null, groups: null }
+  
+  const groups = layerGroups?.sort(byDisplayOrder)
+    .map((group) => (<LegendGroup {...group} />))
 
   // TODO: handle and display loading and error states.
 
@@ -59,7 +31,7 @@ function RouteComponent() {
           top: '40px',
           left: '0',
         }}>
-        { layers ? layers.map((l) => <LayerController {...l} />) : null }
+        { groups }
       </MapView>
       <MapControl style={{ minHeight: '40%' }}>
         <Outlet />
@@ -68,7 +40,6 @@ function RouteComponent() {
       <MapControl>
         <Legend loading={loading}>
           {error?.message}
-          { groups }
         </Legend>
       </MapControl>
     </MapControlOverlay>
---
+++ src/features/map/components/layer-group-controller.tsx
@@ -0,0 +1,35 @@
+import type { OrderedLayerGroup } from "@/types/layers";
+import LegendGroup from "./legend-group";
+import LayerController from "./layer-controller";
+import { byDisplayOrder } from "@/lib/GraphQLProvider";
+import { useRef } from "react";
+import { createPortal } from "react-dom";
+
+
+export default function LayerGroupController(props: OrderedLayerGroup) {
+  const groupRef = useRef<HTMLDivElement>(null)
+  const { layers, ...group } = props
+  layers.sort(byDisplayOrder)
+  const layerNodes = layers.map((layer) => 
+    <LayerController
+      layerId={layer.layerId}
+      activeByDefault={layer.activeByDefault}
+      groupNode={groupRef.current}
+    />
+  )
+
+  const legendNode = document.getElementById('legend')
+
+  return (
+    <>
+      { layerNodes }
+      {
+        legendNode
+          ? createPortal(
+            <LegendGroup {...group} ref={groupRef} />,
+            legendNode
+          ) : null
+      }
+    </>
+  )
+}
