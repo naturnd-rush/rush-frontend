60cab1e2e582ce829b7bca9229efc0de3d35194c
Doug Johnson <43759986+doug-stormtree@users.noreply.github.com>
2026-01-29 13:48:06 -0800

    Feature: Tooltips (#37)
    * Add tooltip binding.
    
    * Add tooltip CSS styles.
    
    * Use custom centroid position for tooltips.
    
    * Increase CSS selector priority.
    
    * Replace undefined marker with opacity 0.
    
    * Move tooltip css to new backend file

--- src/features/map/utils/leaflet-functions.ts
+++ src/features/map/utils/leaflet-functions.ts
@@ -1,10 +1,10 @@
-import { divIcon, Layer, marker, type LatLngExpression } from "leaflet";
+import { divIcon, Layer, marker, tooltip, type LatLngExpression } from "leaflet";
 import type { Feature, Point } from "geojson";
 
 export function pointToLayer(feature: Feature<Point>, coords: LatLngExpression) {
   const markerOpts = feature?.properties?.__pointDivIconStyleProps
     ? { icon: divIcon(feature.properties.__pointDivIconStyleProps) }
-    : undefined
+    : { opacity: 0 }
 
   return marker(coords, markerOpts)
 }
@@ -13,7 +13,28 @@ export function bindFeaturePopup(feature: Feature<Point>, layer: Layer) {
   if (feature?.properties?.__hasPopup) {
     layer.bindPopup(
       feature.properties.__popupHTML,
-      feature.properties.__popupOptions
+      feature.properties.__popupOptions,
     )
   }
+}
+
+export function bindFeatureTooltip(feature: Feature<Point>, layer: Layer) {
+  if (feature?.properties?.__hasTooltip) {
+    const {
+      __tooltipOptions,
+      __tooltipHTML,
+      __tooltipLat,
+      __tooltipLng
+    } = feature.properties
+    const leafletTooltip = tooltip(__tooltipOptions)
+    leafletTooltip.setLatLng([__tooltipLat,__tooltipLng])
+    leafletTooltip.setContent(__tooltipHTML)
+
+    layer.bindTooltip(leafletTooltip)
+  }
+}
+
+export function onEachFeature(feature: Feature<Point>, layer: Layer) {
+  bindFeaturePopup(feature, layer)
+  bindFeatureTooltip(feature, layer)
 }
