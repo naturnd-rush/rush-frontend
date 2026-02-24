8f1c5d077fb1ef46b0ad37883a3a73ea81f078eb
Doug Johnson <doug@stormtree.ca>
2026-02-24 14:57:38 -0800

    Homepage brightness, Panel margin fix, switch colors.

--- src/components/switch.tsx
+++ src/components/switch.tsx
@@ -2,6 +2,8 @@ import { styled } from "@linaria/react"
 import type { InputHTMLAttributes } from "react"
 
 const StyledSwitch = styled.input`
+  --switch-active-color: #3182ce;
+  --switch-inactive-color: #CBD5E0;
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
@@ -10,11 +12,11 @@ const StyledSwitch = styled.input`
   width: 2em;
   height: 1em;
   box-sizing: content-box;
-  border: 1px solid;
+  //border: 1px solid;
   border-radius: 1em;
   vertical-align: text-bottom;
   margin: auto;
-  color: inherit;
+  background-color: var(--switch-inactive-color);
   cursor: pointer;
 
   &:before {
@@ -27,13 +29,17 @@ const StyledSwitch = styled.input`
     width: 0.7em;
     height: 0.7em;
     margin: 0 0.15em;
-    border: 1px solid;
+    //border: 1px solid;
     border-radius: 50%;
-    background: currentcolor;
+    background: white;
   }
 
   &:checked::before {
     left: 1em;
+  }
+
+  &:checked {
+    background-color: var(--switch-active-color);
   }
 `
 export default function Switch(props: InputHTMLAttributes<HTMLInputElement>) {
