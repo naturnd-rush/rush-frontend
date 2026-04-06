dd61f46afc0020b2ce2851f1181b277d0834a10e
Doug Johnson <doug@stormtree.ca>
2026-04-05 17:47:52 -0700

    Add page_view Analytics

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -2,11 +2,13 @@ import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { Toaster } from 'react-hot-toast'
 import NavBar from '@/features/nav-bar/components/nav-bar'
+import AnalyticsPageView from '@/features/analytics/components/AnalyticsPageView'
 
 export const Route = createRootRoute({
   component: () => (
     // #root element is styled in index.css
     <>
+      <AnalyticsPageView />
       <NavBar />
       <Outlet />
       <TanStackRouterDevtools />
--- src/lib/FirebaseProvider.tsx
+++ src/lib/FirebaseProvider.tsx
@@ -1,14 +1,14 @@
 import { createContext, useContext } from 'react';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics, type Analytics } from "firebase/analytics";
+import { initializeApp, type FirebaseOptions } from "firebase/app";
+import { initializeAnalytics, type Analytics } from "firebase/analytics";
 import { Database, getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyA5ZmTCXd9DcX-1Zkp2YT5ygZuNcOgr3l0",
   authDomain: "rush-v3-prod.firebaseapp.com",
   databaseURL: "https://rush-v3-prod-default-rtdb.firebaseio.com",
@@ -17,7 +17,7 @@ const firebaseConfig = {
   messagingSenderId: "453797454383",
   appId: "1:453797454383:web:da51a9bf7c7da3b61a1cda",
   measurementId: "G-ZNKEHL9HL5"
-};
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -31,7 +31,8 @@ type FirebaseContextType = {
 const FirebaseContext = createContext<FirebaseContextType>({ analytics: undefined, database: undefined })
 
 export function FirebaseProvider({ children }: React.PropsWithChildren) {
-  const analytics = getAnalytics(app);
+  // Initialize Analytics without automatic page view on first load, manually sent in '@/app/routes/app/__root.tsx'
+  const analytics = initializeAnalytics(app, {config: { send_page_view: 'false' }});
   const database = getDatabase(app);
 
   return (
