c5ec4c3f584af4ef2c0449cef5bc9c78b29e11ee
Doug Johnson <doug@stormtree.ca>
2026-07-28 16:39:49 -0700

    Desktop ContentPanel with tab menu

--- src/features/content/components/content-panel.tsx
+++ src/features/content/components/content-panel.tsx
@@ -2,9 +2,9 @@ import { Link, useNavigate } from "@tanstack/react-router"
 import { styled } from "@linaria/react"
 import Panel, { PanelCloseButton, PanelContent } from "@/components/panel"
 import type { ContentProps } from "./content-container"
-import { Button, Tabs, Link as ChakraLink } from "@chakra-ui/react"
+import { Tabs, Link as ChakraLink } from "@chakra-ui/react"
 import Loadable from "@/components/loadable"
-import type { Tab } from "@/types/topic"
+import React, { useRef } from "react"
 
 const ContentText = styled.div`
   color: black;
@@ -41,23 +41,6 @@ export default function ContentPanel(props: ContentProps) {
   )
 }
 
-const StyledMenuButton = styled(Button)`
-  font-family: Figtree, sans-serif;
-  font-size: 1.125rem;
-  font-weight: 500;
-  //color: rgb(26, 32, 44);
-`
-
-type TabButtonProps = {
-  icon?: Tab['icon'],
-  title: Tab['title']
-}
-const ActiveTabButton = ({icon, title}: TabButtonProps) => (
-  <StyledMenuButton variant='ghost'>
-    {icon}{title}
-  </StyledMenuButton>
-)
-
 type ContentTabsMenuProps = Pick<ContentProps, 'tabs' | 'activeTabId'>
 
 function ContentTabsMenu(props: ContentTabsMenuProps) {
@@ -69,19 +52,64 @@ function ContentTabsMenu(props: ContentTabsMenuProps) {
 
   const navigate = useNavigate({ from: '/app/$topicId/$tabId' })
 
+  // scroll panel tab contents to top on tab change
+  const tabMenuRef = useRef<HTMLDivElement | null>(null);
+  const handleTabChangeScroll = () => {
+    const currentTabMenu = tabMenuRef.current
+    if (currentTabMenu) {
+      const nextSibling = currentTabMenu.nextElementSibling
+      if (nextSibling) nextSibling.scrollTo(0,0)
+    }
+  }
+
   return (
     <Tabs.Root
+      ref={tabMenuRef}
       value={activeTab.id}
       onValueChange={({ value }) => navigate({ to: '/app/$topicId/$tabId', params: { tabId: value }})}
+      variant='enclosed'
+      display='inline-flex'
+      paddingY='0.5rem'
+      className="light"
     >
-      <Tabs.List overflowX='scroll' overflowY='hidden'>
-        { props.tabs.map((tab) => 
-          <Tabs.Trigger key={tab.id} value={tab.id} flexShrink='0' asChild>
-            <ChakraLink>
-              {tab.icon}
-              {tab.title}
-            </ChakraLink>
-          </Tabs.Trigger>
+      <Tabs.List flex='1' overflowY='hidden' scrollbarWidth='none' css={{'::-webkit-scrollbar': { display: 'none'}}}>
+        { props.tabs.map((tab) => {
+          // handle bringing tab into view on click
+          const ref = React.createRef<HTMLButtonElement | null>();
+          const handleTabClick = () => {
+            if (ref.current !== null) {
+              ref.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'end',
+                inline: 'center',
+              })
+            }
+
+            handleTabChangeScroll()
+          }
+
+          return (
+            <Tabs.Trigger
+              key={tab.id}
+              value={tab.id}
+              ref={ref}
+              onClick={handleTabClick}
+              flex='1 0 auto'
+              asChild
+            >
+              <ChakraLink
+                focusRing='none' 
+                focusVisibleRing='mixed'
+                fontFamily='Figtree, sans-serif'
+                fontSize='1rem'
+                fontWeight='500'
+              >
+                {tab.icon}
+                {tab.title}
+              </ChakraLink>
+            </Tabs.Trigger>
+          )
+        }
         )}
       </Tabs.List>
     </Tabs.Root>
